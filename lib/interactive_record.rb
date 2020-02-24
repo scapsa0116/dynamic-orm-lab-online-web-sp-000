96e6ced0e130617312d3a7a72071cf2f0c3d6b59
Stela Capsa <stela.capsa@gmail.com>
2020-02-24 13:05:52 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -52,4 +52,9 @@ def initialize(options={})
   sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
   DB[:conn].execute(sql, name)
 end
+
+def self.find_by(hash)
+    sql = "SELECT * FROM #{self.table_name} WHERE #{hash.keys[0].to_s} = '#{hash.values[0].to_s}'"
+    DB[:conn].execute(sql)
+  end
 end
