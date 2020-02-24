b7e936903f4c9ca2a5a35dd86a77037f0aeb01a0
Stela Capsa <stela.capsa@gmail.com>
2020-02-24 12:57:14 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,8 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end 
+
 end
