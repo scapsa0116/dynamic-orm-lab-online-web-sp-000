4d23e32f4a952c392ee14def26916ff44f464bf2
Stela Capsa <stela.capsa@gmail.com>
2020-02-24 12:20:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,7 +9,16 @@ def self.table_name
 end 
 
 def self.column_names
-  DB[:comm]
-  
-end 
+    DB[:conn].results_as_hash = true
+
+    sql = "pragma table_info('#{table_name}')"
+
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+    table_info.each do |row|
+      column_names << row["name"]
+    end
+    column_names.compact
+  end
+
 end
