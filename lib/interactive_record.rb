196f4fac6144953d711da9e37bbd7be64cbf3e4f
Stela Capsa <stela.capsa@gmail.com>
2020-02-24 13:02:27 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -35,6 +35,10 @@ def initialize(options={})
   end 
   
   def values_for_insert
-    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert} VALUES (#{"
-
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
+  end
 end
